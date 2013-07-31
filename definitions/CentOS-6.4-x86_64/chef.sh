a681614f094a0e30a035ff5c574192edb5cc8c34
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-31 16:22:31 -0600

    Ugh. Work around HTTPS issues so I can build the boxes at work.

--- definitions/CentOS-6.4-x86_64/chef.sh
+++ definitions/CentOS-6.4-x86_64/chef.sh
@@ -1,2 +1,4 @@
 # Install Chef
-curl -L https://www.opscode.com/chef/install.sh | bash
+# FIXME? Ugly workaround so veewee can build on our intranet where HTTPS stuff
+# won't work by default.
+curl -Lk https://www.opscode.com/chef/install.sh | sed -e "s/https/http/" | bash
